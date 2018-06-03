c1896f8bcd2a21c5652a8446ff089a208c3f47f5
T L <tom.kennedy.lee@gmail.com>
2018-06-03 02:21:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ function happyHolidays(string) {
   return "Happy holidays!"
 }
 
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`
+}
+
 function happyHolidaysTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 } 
