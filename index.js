69809cea2557fe56ab5e3134c4470057e798b6c6
T L <tom.kennedy.lee@gmail.com>
2018-06-03 02:24:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 } 
 
-function holidayCountdown(days, holidays) {
+function holidayCountdown(holiday, days) {
   return `It's ${days} days until ${holiday}!`
 }
